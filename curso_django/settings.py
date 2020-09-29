bc28320c020cf0ff6bdc239ffc7d2693bd3cd90b
Pedro Henrique <p13dr0h@gmail.com>
2020-09-29 00:15:39 -0300

    teste env

--- curso_django/settings.py
+++ curso_django/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'inz^#rovv&1unye_mj)1d11d++^56cxi&1jj21&$e5mp*)j6dx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", cast=bool)
 
 ALLOWED_HOSTS = []
 
